67f9c9e2c74725e7246adda9dd10a014d5f63988
MansiMaheshwari <mansi.maheshwari@nordstrom.com>
2023-05-07 18:03:57 -0700

    encoder/decoder

--- chatGptCode.py
+++ chatGptCode.py
@@ -1,11 +1,33 @@
+import string
 
 # Read shakespear File
 with open('tinyShakespearData', 'r') as f:
     content = f.read()
-    print("length of dataset in characters: ", len(content))
 
+    # find all chars in the shakespear text
     chars = sorted(list(set(content)))
-    print("chars: ", ''.join(chars))
-    print(len(chars))
+
+    # create a mapping from chars to integers
+    stoi = {}
+    itos = {}
+    # enumerate chars (each char is associated with a number)
+    integer_chars = enumerate(chars)
+    # The loop assigns each pair of (index, character) in integer_chars to the variables i and c, respectively.
+    for i, c in integer_chars:
+        stoi[c] = i
+        itos[i] = c
+    print("stoi:", stoi)
+    print("itos:", itos)
 
 
+    # encoder
+    def encode(string):
+        return [stoi[char] for char in string]
+
+    # decoder
+    def decode(l):
+        return ''.join([itos[i] for i in l])
+
+
+    print(encode("hii there"))
+    print(decode(encode("hii there")))
